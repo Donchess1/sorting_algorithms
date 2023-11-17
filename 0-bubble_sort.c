b1f18f82330d37579c76c672364d6428fef8700d
Donchess1 <stepabod@yahoo.com>
2023-11-17 17:03:48 +0000

    bubble_sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+/**
+ * bubble_sort - sors the interger of an array
+ * 
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+
+void bubble_sort(int *array, size_t size) 
+{
+	size_t i, j;
+       	size_t temp;
+	for(i = 0; i < size - 1; i++)
+	{
+		for(j = 0; j < size - i -1; j++)
+		{
+			if(array[j+1] < array[j])
+			{
+				 temp = array[j];
+				 array[j] = array[j+1];
+				 array[j+1] = temp;
+			}
+			print_array(array, size);
+		}
+	}
+}
